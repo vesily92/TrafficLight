d0db0d8b2e61fafa00326b0e829916f5ec709e8b
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-08-17 18:46:03 +0300

    Layout setup

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 100
+        yellowLightView.layer.cornerRadius = 100
+        greenLightView.layer.cornerRadius = 100
+        startButton.layer.cornerRadius = 20
+        
+        redLightView.alpha = 1
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 03
     }
 
-
+    @IBAction func changeTrafficLights() {
+    }
+    
 }
 
